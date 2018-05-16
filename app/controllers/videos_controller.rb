ff1b80b85f9d76ab050fa46aad031b3ceee3aa5f
davidliang781205 <davidliang781205@gmail.com>
2018-05-15 23:42:11 -0700

    Updated gemlock, cleaned up code

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,7 +4,6 @@ class VideosController < ApplicationController
     if params['v'].present? && current_user
       @v = params['v']
       @v_info = youtube_client.list_videos('snippet,liveStreamingDetails,statistics', id: @v)
-      # @v_chat = youtube_client.list_live_chat_messages(@v_info.items.first.live_streaming_details.active_live_chat_id, 'snippet,authorDetails')
       @token = access_token
     end
   end
